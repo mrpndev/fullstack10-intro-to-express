77ded9618c341925e4c936b1a5d21b7ecdd6754d
Paul Niemczyk <mrpndev@gmail.com>
2026-03-23 20:36:18 -0400

    intro to express complete

--- app.js
+++ app.js
@@ -12,7 +12,24 @@ const HOST = "127.0.0.1"
 app.get("/test", (req, res) => {
 	console.log("/test endpoint has been hit")
 	console.log(req)
+	// response object gives us access to its different method
+	// .send() method allows to send buffer, JSON, text, or HTML response
+	res.send("<h1 style='color: red'>I'm here, what do you want?!</h1>")
 })
+
+/* 
+	? Challenge
+	* create a new GET endpoint at /
+	* within the callback, send a response
+	* the response should send a 201 status code (explicitly)
+	* the response should also send a JSON object with two properties:
+		* message: "This is the / endpoint"
+		* status: "The server is running"
+	* HINT: you might need to search the docs for the method(s) to do this with
+
+	! HARD MODE
+	* instead of hardcoding the endpoint, try and pull it from the request?????
+*/
 
 // function that creates a listener on said socket (host + port)
 app.listen(PORT, HOST, () => {
